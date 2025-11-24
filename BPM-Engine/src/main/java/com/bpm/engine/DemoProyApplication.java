1a10bfa462ea9c5dedc203cac57388a2f69c335a
Daniel MC <danielcarrillo3200@gmail.com>
2025-11-24 12:01:12 -0500

    usuario camunda

--- BPM-Engine/src/main/java/com/bpm/engine/DemoProyApplication.java
+++ BPM-Engine/src/main/java/com/bpm/engine/DemoProyApplication.java
@@ -1,7 +1,10 @@
 package com.bpm.engine;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.*;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class DemoProyApplication {
